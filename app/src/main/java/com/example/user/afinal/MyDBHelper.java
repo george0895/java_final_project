b50bbb0a06367677b4178de73fb6acdc5354b737
ian87815 <bangbangbang88888@gmail.com>
2018-12-26 01:37:57 +0800

    加頁面

--- app/src/main/java/com/example/user/afinal/MyDBHelper.java
+++ app/src/main/java/com/example/user/afinal/MyDBHelper.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+
 public class MyDBHelper extends  SQLiteOpenHelper {
     private static final String name = "mbdatabase.db";
     private static final int version = 1;
